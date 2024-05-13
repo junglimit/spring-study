867e8c481204381d9c669ebea96c58121c75a5b6
junglimit <jaehan_jjh@naver.com>
2024-05-13 17:04:23 +0900

    제어의 역전(IoC), 의존성 주입(DI)

---
+++ src/test/java/com/study/springstudy/chap03/HoteDiTest.java
@@ -0,0 +1,18 @@
+package com.study.springstudy.chap03;
+
+import com.study.springstudy.chap03.config.HotelManager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HoteDiTest {
+
+    @Test
+    void diTest() {
+
+        HotelManager manager = new HotelManager();
+        Hotel hotel = manager.hotel();
+        hotel.inform();
+    }
+
+}
