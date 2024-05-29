1cb4ed35da53e3052f91515706ec6e2d0687e02f
junglimit <jaehan_jjh@naver.com>
2024-05-29 09:50:04 +0900

    댓글 무한스크롤 구현

--- src/main/resources/static/assets/js/getReply.js
+++ src/main/resources/static/assets/js/getReply.js
@@ -1,5 +1,4 @@
-
-import { BASE_URL } from "./reply.js";
+import { BASE_URL } from './reply.js';
 
 function getRelativeTime(createAt) {
     // 현재 시간 구하기
@@ -7,9 +6,10 @@ function getRelativeTime(createAt) {
     // 등록 시간 날짜타입으로 변환
     const past = new Date(createAt);
 
-    // 사이 시간 구하기
+    // 사이 시간 구하기 (밀리초)
     const diff = now - past;
     // console.log(diff);
+
     const seconds = Math.floor(diff / 1000);
     const minutes = Math.floor(diff / 1000 / 60);
     const hours = Math.floor(diff / 1000 / 60 / 60);
@@ -32,54 +32,50 @@ function getRelativeTime(createAt) {
     }
 }
 
-function renderPage({begin, end, prev, next, pageInfo}) {
+
+function renderPage({ begin, end, pageInfo, prev, next }) {
     let tag = '';
 
-    // 페이지 번호 태그 만들기
+    // prev 만들기
+    if (prev) tag += `<li class='page-item'><a class='page-link page-active' href='${begin - 1}'>이전</a></li>`;
 
-    if (prev) {
-        tag += `<li class='page-item'><a class='page-link page-custom' href='${begin - 1}'>prev</a></li>`;
-    }
-    for (let i = begin; i<=end; i++) {
+    // 페이지 번호 태그 만들기
+    for (let i = begin; i <= end; i++) {
 
         let active = '';
         if (pageInfo.pageNo === i) active = 'p-active';
 
-        tag +=`<li class='page-item ${active}'><a class='page-link page-custom' href='${i}'>${i}</a></li>`;
-
+        tag += `
+      <li class='page-item ${active}'>
+        <a class='page-link page-custom' href='${i}'>${i}</a>
+      </li>`;
     }
 
-
-
-    if (next) {
-        tag += `<li class='page-item'><a class='page-link page-custom' href='${end + 1}'>next</a></li>`;
-    }
-
-
-
+    // next 만들기
+    if (next) tag += `<li class='page-item'><a class='page-link page-active' href='${end + 1}'>다음</a></li>`;
 
     // 페이지 태그 ul에 붙이기
     const $pageUl = document.querySelector('.pagination');
     $pageUl.innerHTML = tag;
-
 }
 
-export function renderReplies({pageInfo, replies}) {
-
+export function renderReplies({ pageInfo, replies }) {
     // 댓글 수 렌더링
     document.getElementById('replyCnt').textContent = pageInfo.totalCount;
 
     // 댓글 목록 렌더링
     let tag = '';
     if (replies && replies.length > 0) {
-        replies.forEach(({ rno, writer, text, createAt }) => {
+        replies.forEach(({ reply_no: rno, writer, text, createAt }) => {
             tag += `
         <div id='replyContent' class='card-body' data-reply-id='${rno}'>
             <div class='row user-block'>
                 <span class='col-md-3'>
                     <b>${writer}</b>
                 </span>
-                <span class='offset-md-6 col-md-3 text-right'><b>${getRelativeTime(createAt)}</b></span>
+                <span class='offset-md-6 col-md-3 text-right'><b>${getRelativeTime(
+                createAt
+            )}</b></span>
             </div><br>
             <div class='row'>
                 <div class='col-md-9'>${text}</div>
@@ -91,8 +87,6 @@ export function renderReplies({pageInfo, replies}) {
         </div>
         `;
         });
-
-
     } else {
         tag = `<div id='replyContent' class='card-body'>댓글이 아직 없습니다! ㅠㅠ</div>`;
     }
@@ -104,13 +98,13 @@ export function renderReplies({pageInfo, replies}) {
 
 }
 
-// 서버에서 댓글목록 가져오는 비동기 요청 함수
+// 서버에서 댓글 목록 가져오는 비동기 요청 함수
 export async function fetchReplies(pageNo=1) {
-
     const bno = document.getElementById('wrap').dataset.bno; // 게시물 글번호
 
     const res = await fetch(`${BASE_URL}/${bno}/page/${pageNo}`);
     const replyResponse = await res.json();
+    // { pageInfo: {}, replies: [] }
 
     // 댓글 목록 렌더링
     renderReplies(replyResponse);
@@ -118,14 +112,99 @@ export async function fetchReplies(pageNo=1) {
 
 // 페이지 클릭 이벤트 생성 함수
 export function replyPageClickEvent() {
-    // const $next = document.querySelector('.next');
-    // const $prev = document.querySelector('.prev');
-    document.querySelector('.pagination').addEventListener('click', (e) => {
+
+    document.querySelector('.pagination').addEventListener('click', e => {
         e.preventDefault();
-        console.log(e.target);
         // console.log(e.target.getAttribute('href'));
         fetchReplies(e.target.getAttribute('href'));
+    });
 
-
-    })
 }
+
+
+// =============== 무한 스크롤 전용 함수 ============= //
+
+let currentPage = 1; // 현재 무한스크롤시 진행되고 있는 페이지 번호
+let isFetching = false; // 데이터를 불러오는 중에는 더 가져오지 않게 제어하기 위한 논리변수
+
+function appendReplies({ replies }) {
+
+
+    // 댓글 목록 렌더링
+    let tag = '';
+    if (replies && replies.length > 0) {
+        replies.forEach(({ reply_no: rno, writer, text, createAt }) => {
+            tag += `
+        <div id='replyContent' class='card-body' data-reply-id='${rno}'>
+            <div class='row user-block'>
+                <span class='col-md-3'>
+                    <b>${writer}</b>
+                </span>
+                <span class='offset-md-6 col-md-3 text-right'><b>${getRelativeTime(
+                createAt
+            )}</b></span>
+            </div><br>
+            <div class='row'>
+                <div class='col-md-9'>${text}</div>
+                <div class='col-md-3 text-right'>
+                    <a id='replyModBtn' class='btn btn-sm btn-outline-dark' data-bs-toggle='modal' data-bs-target='#replyModifyModal'>수정</a>&nbsp;
+                    <a id='replyDelBtn' class='btn btn-sm btn-outline-dark' href='#'>삭제</a>
+                </div>
+            </div>
+        </div>
+        `;
+        });
+    } else {
+        tag = `<div id='replyContent' class='card-body'>댓글이 아직 없습니다! ㅠㅠ</div>`;
+    }
+
+    document.getElementById('replyData').innerHTML += tag;
+
+}
+
+
+// 서버에서 댓글 데이터를 페칭
+export async function fetchInfScrollReplies(pageNo=1) {
+
+    if(isFetching) return; // 서버에서 데이터를 가져오는 중이면 return
+
+    isFetching = true;
+
+    const bno = document.getElementById('wrap').dataset.bno; // 게시물 글번호
+    const res = await fetch(`${BASE_URL}/${bno}/page/${pageNo}`);
+    const replyResponse = await res.json();
+
+    if(pageNo === 1){
+        // 댓글 수 렌더링
+        document.getElementById('replyCnt').textContent = replyResponse.pageInfo.totalCount;
+        // 초기 댓글 리셋
+        document.getElementById('replyData').innerHTML = '';
+    }
+
+    // 댓글 목록 렌더링
+    // console.log(replyResponse);
+    appendReplies(replyResponse);
+    currentPage = pageNo;
+    isFetching = false;
+}
+
+// 스크롤 이벤트 핸들러 함수
+function scrollHandler(e) {
+
+    // 스크롤이 최하단부로 내려갔을 때만 이벤트 발생시켜야 함
+    //  현재창에 보이는 세로길이 + 스크롤을 내린 길이 >= 브라우저 전체 세로길이
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight + 200) {
+        // console.log(e);
+        // 서버에서 데이터를 비동기로 불러와야 함
+        fetchInfScrollReplies(currentPage + 1);
+    }
+
+
+}
+
+// 무한 스크롤 이벤트 생성 함수
+export function setupInfiniteScroll() {
+    window.addEventListener('scroll', scrollHandler);
+}
+
+
--- src/main/resources/static/assets/js/reply.js
+++ src/main/resources/static/assets/js/reply.js
@@ -1,6 +1,6 @@
-import { fetchReplies, replyPageClickEvent } from "./getReply.js";
+
+import { fetchInfScrollReplies, setupInfiniteScroll } from "./getReply.js";
 import { fetchReplyPost } from "./postReply.js";
-import {deleteReply} from "./deleteReply.js";
 
 // ====== 전역 변수 ========
 export const BASE_URL = 'http://localhost:8383/api/v1/replies';
@@ -8,16 +8,19 @@ export const BASE_URL = 'http://localhost:8383/api/v1/replies';
 // ====== 실행 코드 ========
 
 // 댓글 목록 서버에서 불러오기
-fetchReplies();
+// fetchReplies();
+fetchInfScrollReplies(); // 일단 1페이지 데이터 그려놓기
+setupInfiniteScroll(); // 무한 스크롤 이벤트 등록
 
 // 댓글 작성 이벤트 등록
-document.getElementById('replyAddBtn').addEventListener('click',e=>{
+document.getElementById('replyAddBtn').addEventListener('click', e => {
     // 댓글 등록 로직
     fetchReplyPost();
 });
 
 // 댓글 페이지 클릭이벤트 등록
-replyPageClickEvent();
+// replyPageClickEvent();
 
-// 댓글 삭제 함수
-deleteReply();
+
+
+
