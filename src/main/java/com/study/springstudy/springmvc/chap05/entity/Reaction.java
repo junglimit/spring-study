08040c99c14945be3a22811182d570e3afa057c4
junglimit <jaehan_jjh@naver.com>
2024-06-04 09:58:18 +0900

    좋아요 , 싫어요 기능

---
+++ src/main/java/com/study/springstudy/springmvc/chap05/entity/Reaction.java
@@ -0,0 +1,20 @@
+package com.study.springstudy.springmvc.chap05.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reaction {
+
+    private long reactionId;
+    private long boardNo;
+    private String account;
+    private ReactionType reactionType;
+    private LocalDateTime reactionDate;
+
+}
